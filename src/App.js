3c3264a955c21eda283a5e5c81850e9e5f170545
sagargautam10 <sagargautam0210@gmail.com>
2024-08-24 22:07:18 +0545

    huhhh

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import About from "./pages/About";
 import Menu from "./pages/Menu";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/PageNotFound";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-       <BrowserRouter>
-         <Routes>
-           <Route path="/" element={<Home />} />
-           <Route path="/about" element={<About />} />
-           <Route path="/menu" element={<Menu />} />
-           <Route path="/contact" element={<Contact />} />
-           <Route path="*" element={<PageNotFound />} />
-         </Routes>
-       </BrowserRouter>
+      <BrowserRouter basename="/Web-cafe-mui">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 export default App;
